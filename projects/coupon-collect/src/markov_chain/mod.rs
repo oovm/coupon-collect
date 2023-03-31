d22394943442bc1ccc12b344db2577e7e7c8c1cf
Aster <aster@vers.site>
2023-03-31 20:17:41 +0800

    Find all transition

--- projects/coupon-collect/src/markov_chain/mod.rs
+++ projects/coupon-collect/src/markov_chain/mod.rs
@@ -1,7 +1,7 @@
 use std::iter::from_generator;
-use itertools::Itertools;
 use ndarray::Array2;
-use num::{BigInt, One, Zero};
+
+use num::{BigInt, One};
 use num::rational::Ratio;
 
 pub struct MarkovChain {
@@ -13,20 +13,22 @@ pub struct MarkovChain {
     weights: Vec<Ratio<BigInt>>,
 }
 
+#[derive(Debug)]
+pub struct StateTransition {
+    transition: Vec<usize>,
+    probability: Ratio<BigInt>,
+}
+
 
 impl MarkovChain {
-    pub fn new(group: usize) -> Self {
-        Self {
-            group,
-            target: vec![],
-            weights: vec![],
-        }
+    /// Count kinds of cards
+    pub fn count_kind(&self) -> usize {
+        self.weights.len()
     }
-    pub fn define_cards(&mut self, needs: usize, weight: Ratio<BigInt>) {
-        self.target.push(needs);
-        self.weights.push(weight);
+    /// Count cards in a deck
+    pub fn count_deck(&self) -> usize {
+        self.group
     }
-
     /// Give the weight of each option
     pub fn get_weights(&self) -> Vec<Ratio<BigInt>> {
         self.weights.clone()
@@ -37,85 +39,79 @@ impl MarkovChain {
         let sum = self.weights.iter().sum::<Ratio<BigInt>>();
         self.weights.iter().map(|x| x / sum.clone()).collect()
     }
-    // pub fn get_states(&self) -> impl Iterator<Item=Vec<usize>> {
-    //    unimplemented!()
-    // }
     pub fn transfer_matrix(&self) -> Array2<Ratio<BigInt>> {
         todo!();
         // let mut matrix = Array2::<f64>::zeros((self.weights.len(), self.weights.len()));
     }
-    fn standardize(&self) -> StandardizedMarkovChain {
-        let mut probabilities = self.get_probabilities();
-        StandardizedMarkovChain {
-            group: self.group,
-            target: self.target.clone(),
-            probabilities,
-        }
-    }
-}
-
-#[derive(Debug)]
-pub struct StateTransition {
-    transition: Vec<usize>,
-    probability: Ratio<BigInt>,
-}
-
-pub struct StandardizedMarkovChain {
-    /// Number of cards per pack
-    group: usize,
-    /// Target number of cards
-    target: Vec<usize>,
-    /// Number of cards in the deck, sum must 1
-    probabilities: Vec<Ratio<BigInt>>,
-}
-
-impl StandardizedMarkovChain {
-    pub fn get_probability(&self, delta: &[usize]) -> Ratio<BigInt> {
-        let mut probability = Ratio::one();
-        for (i, &d) in delta.iter().enumerate() {
-            probability *= self.probabilities[i].pow(d as i32);
-        }
-        probability
-    }
-
-    pub fn get_states(&self) -> impl Iterator<Item=Vec<usize>> {
-        // kinds of all cards
-        let items = self.target.len();
-        // cards in each pack
-        let group = self.group;
-        // eg, group = 3, items = 3
-        // 0 0 0
-        // 1,1,1
-        // 0,1,2 | 0,2,1 | 1,0,2 | 1,2,0 | 2,0,1 | 2,1,0
-        // 0,0,3 | 0,3,0 | 3,0,0
-        for i in 0..=group {
-            let mut state = vec![0; items];
-            state[0] = i;
-            for j in 0..=(group - i) {
-                state[1] = j;
-                state[2] = group - i - j;
+    pub fn get_transition_states(&self) -> impl Iterator<Item=Vec<usize>> {
+        let kind = self.count_kind();
+        let deck = self.count_deck();
+        let mut state = vec![0; kind];
+        state[0] = deck;
+        // eg. deck = 3, kind = 2
+        // (3, 0), (2, 1), (1, 2), (0, 3)
+        from_generator(move || {
+            yield state.clone();
+            loop {
+                let mut next = state.clone();
+                let mut i = 0;
+                while i < kind - 1 {
+                    if next[i] > 0 {
+                        next[i] -= 1;
+                        next[i + 1] += 1;
+                        break;
+                    }
+                    i += 1;
+                }
+                if i == kind - 1 {
+                    break;
+                }
+                state = next;
                 yield state.clone();
             }
+        })
+    }
+    pub fn get_transition(&self, delta: &[usize], probabilities: &[Ratio<BigInt>]) -> StateTransition {
+        let mut transition = vec![0; self.count_kind()];
+        let mut probability = Ratio::one();
+        for (i, &d) in delta.iter().enumerate() {
+            transition[i] = d;
+            probability *= probabilities[i].pow(d as i32);
+        }
+        StateTransition {
+            transition,
+            probability,
         }
     }
-    pub fn get_transition(&self) -> impl Iterator<Item=StateTransition> + '_ {
-        todo!()
+}
+
+
+impl MarkovChain {
+    pub fn new(group: usize) -> Self {
+        Self {
+            group,
+            target: vec![],
+            weights: vec![],
+        }
+    }
+    pub fn define_cards<N>(&mut self, needs: usize, weight: N)
+        where
+            N: Into<Ratio<BigInt>>,
+    {
+        self.target.push(needs);
+        self.weights.push(weight.into());
     }
 }
 
 
 #[test]
 fn test() {
-    let f13 = Ratio::new(BigInt::from(1), BigInt::from(3));
-    let mc = StandardizedMarkovChain {
-        group: 3,
-        target: vec![0, 1, 2],
-        probabilities: vec![f13.clone(), f13.clone(), f13.clone()],
-    };
-    for state in mc.get_states() {
-        println!("{:?}", state);
-    }
-    for state in mc.get_transition() {
-        println!("{:?}", state);
+    let mut mc = MarkovChain::new(3);
+    mc.define_cards(1, BigInt::from(1));
+    mc.define_cards(1, BigInt::from(1));
+    let probabilities = mc.get_probabilities();
+    println!("{:?}", probabilities);
+    for state in mc.get_transition_states() {
+        println!("{:?}", mc.get_transition(&state, &probabilities));
     }
 }
